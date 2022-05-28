ccdacc763e07f4140495e1a6ab953b35398dd91c
Vikash2806 <vikashrajendran2806@gmail.com>
2022-05-28 21:45:24 +0530

    Revert "Feat: Ex-7 add"

--- EXPT7/Ex-7.7.py
+++
@@ -1,9 +0,0 @@
-def transformCase(string):
-    return str(string).upper(), str(string).lower()
-
-
-char = {'a', 'b', 'E', 'f', 'a', 'i', 'o', 'U', 'a'}
-
-result = map(transformCase, char)
-
-print(set(result))
