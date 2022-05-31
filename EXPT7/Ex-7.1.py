da7c3786459f72b81bf7924b936b1c4a470d9af2
Dharsan-1908 <idappadidharsan@gmail.com>
2022-05-31 08:05:05 +0530

    feat: 7.1 new method added

--- EXPT7/Ex-7.1.py
+++ EXPT7/Ex-7.1.py
@@ -7,3 +7,12 @@ def fib(n): return reduce(lambda x, _: x+[x[-1]+x[-2]],
 
 print(fib(5))
 
+# map and lambda method
+
+# def fib(n):
+    # fiblist = [0,1]
+
+    # any(map(lambda _:fiblist.append(sum(fiblist[-2:])),range(2, n)))
+    # return fiblist[:n]
+
+# print(fib(15))
