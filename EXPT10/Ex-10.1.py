8861605b1982f12c89b3cbdfbeb6506bbe3251c6
Dharsan-1908 <idappadidharsan@gmail.com>
2022-06-10 13:46:40 +0530

    Feat: logic programming updated

--- EXPT10/Ex-10.1.py
+++ EXPT10/Ex-10.1.py
@@ -1,13 +1,11 @@
-from pyDatalog import pyDatalog
-
-pyDatalog.create_terms('"Bear","Elephant","cat","big","small","brown","black","gray","size","colour","x","Dark"')
-+size("Bear","big")
-+size("Elephant","big")
-+size("cat","small")
-+colour("Bear","brown")
-+colour("cat","black")
-+colour("Elephant","gray")
-Dark(x) <= (colour(x,"black"))
-Dark(x) <= (colour(x,"brown"))
-
-print(size(x,"big"))
+from pyDatalog import pyDatalog 
+pyDatalog.create_terms('Bear,Elephant,Cat,Black,Gray,Brown,Big,small,size,color,X,Y,dark')
++size("Bear","Big")
++size("Elephant","Big")
++size("Cat","small")
++color("Bear","Brown")
++color("Cat","Black")
++color("Elephant","Gray")
+dark(X) <= (color(X,"Black"))
+dark(X) <= (color(X,"Brown"))
+print(dark(X) &(size(X,'Big')))
