5389be59f5063bb05277f4b131d91e630b0af295
VikashPR <vikashrajendran2806@gmail.com>
2022-06-27 09:03:20 +0530

    Feat(Update): UDP client, server

--- CT3-Practice/Ex2-ClientUDP.py
+++ CT3-Practice/Ex2-ClientUDP.py
@@ -2,7 +2,11 @@ import socket
 
 c = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 
-
 msg = b"Hello world!"
-
 c.sendto(msg,('localhost',8080))
+
+s,addr = c.recvfrom(1024)
+
+print("Server says",s)
+
+c.close()
--- CT3-Practice/Ex2-ServerUDP.py
+++ CT3-Practice/Ex2-ServerUDP.py
@@ -6,4 +6,6 @@ s.bind(("localhost",8080))
 while True:
     print("Waiting for client")
     c,addr = s.recvfrom(1024)
+    msg = b"Response from the server"
+    s.sendto(msg,addr)
     print("Recieved message: ",c,"From",addr)
