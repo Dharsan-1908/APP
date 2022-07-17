3a0d4bef207b0be10a5e2e7389a104c096bf57d1
VikashPR <vikashrajendran2806@gmail.com>
2022-07-17 22:48:41 +0530

    Feat(update): TCP client, server practice

--- SEM/network/tcpClient.py
+++ SEM/network/tcpClient.py
@@ -1,14 +1,30 @@
+# import socket
+
+# c = socket.socket()
+
+# c.connect(("localhost",8088))
+
+# name = input("Enter your name: ")
+
+# c.send(name.encode())
+
+# recvData = c.recv(1024)
+# print(str(recvData.decode('utf-8')))
+
+# c.close()
+
 import socket
 
-c = socket.socket()
+client = socket.socket()
 
-c.connect(("localhost",8088))
+client.connect(("localhost",8080))
 
-name = input("Enter your name: ")
+dataStr = b"Hello cat!"
 
-c.send(name.encode())
+client.send(dataStr)
 
-recvData = c.recv(1024)
-print(str(recvData.decode('utf-8')))
+recvData = client.recv(1024)
 
-c.close()
+print(str(recvData.decode("utf-8")))
+
+client.close()
--- SEM/practice.py
+++ SEM/practice.py
@@ -1,6 +1,22 @@
-arr = [1,2,3,4,5,6,7,8,9]
-
-len = len(arr)
-
-for i in range(1,len+1,2):
-    print(i)
+class Student:
+    avg = 0
+    def __init__(self, stNo, m1, m2, m3, m4, m5):
+        self.studentNum = stNo
+        self.mark1, self.mark2, self.mark3, self.mark4, self.mark5 = m1,
+        m2, m3, m4, m5
+    def avgMark(self):
+        self.avg = (self.mark1 + self.mark2 + self.mark3 + self.mark4 +
+                self.mark5) / 5
+def grade(self):
+    if(self.avg >= 80):
+        print("Distinction")
+    elif(self.avg >= 65 and self.avg < 80):
+        print("Merit")
+    elif(self.avg >= 50 and self.avg < 65):
+        print("Pass")
+    else:
+        print("Fail")
+        
+obj = Student("20202", 10, 20, 30, 40, 80)
+print(obj.studentNum)
+obj.grade()
